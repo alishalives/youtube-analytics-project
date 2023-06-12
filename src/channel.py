fb0edfae7ae5435c589890d147c800f7ec4b97d4
ADMIN <alina.velichko1@mail.ru>
2023-06-12 17:26:36 +0300

    homework-2 done

--- src/channel.py
+++ src/channel.py
@@ -10,9 +10,45 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
+        self.__channel = Channel.youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
 
-    def print_info(self) -> None:
+        self.title = self.__channel['items'][0]['snippet']['title']
+        self.description = self.__channel['items'][0]['snippet']['description']
+        self.url = self.__channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.followers_count = self.__channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.__channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.__channel['items'][0]['statistics']['viewCount']
+
+    def print_info(self) -> str:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = Channel.youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
         return json.dumps(channel, indent=2, ensure_ascii=False)
+
+    @property
+    def channel_id(self):
+        """
+        Метод геттер для защищенного поля channel_id
+        """
+        return self._channel_id
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, filename):
+        """
+        Создание json-файла с атрибутами класса
+        """
+        data = {
+            "channel_id": self._channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriber_count": self.followers_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+
